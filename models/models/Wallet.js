c64efd648f58d0109ef0f955f175f85e4c1851a9
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 22:16:38 +0600

    Delete models/models directory

--- models/models/Wallet.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const walletSchema = new mongoose.Schema({
-  userId: String,
-  balance: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model("Wallet", walletSchema);
