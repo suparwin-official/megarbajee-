2a2f8ad95099ffda844e82c10003b1c3c1249af2
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 23:12:45 +0600

    Update rateLimitMiddleware.js

--- middleware/rateLimitMiddleware.js
+++ middleware/rateLimitMiddleware.js
@@ -1,1 +1,19 @@
+const users = new Map();
 
+export const rateLimitMiddleware = (req, res, next) => {
+  const ip = req.ip;
+
+  const current = users.get(ip) || 0;
+
+  if (current > 100) {
+    return res.status(429).json({ message: "Too many requests" });
+  }
+
+  users.set(ip, current + 1);
+
+  setTimeout(() => {
+    users.set(ip, current - 1);
+  }, 1000 * 60);
+
+  next();
+};
