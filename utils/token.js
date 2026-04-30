805847202890477e651daab9778e4277fafecf4a
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 22:18:56 +0600

    Update token.js

--- utils/token.js
+++ utils/token.js
@@ -1,1 +1,11 @@
+const jwt = require("jsonwebtoken");
 
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
+
+module.exports = generateToken;
