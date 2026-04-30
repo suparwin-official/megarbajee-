7fd3007df56f6d41bfdc25e358324234e41a801f
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 23:06:31 +0600

    Update Bet.js

--- models/Bet.js
+++ models/Bet.js
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
 
+const betSchema = new mongoose.Schema({
+  userId: String,
+  game: String,
+  amount: Number,
+  status: {
+    type: String,
+    default: "pending"
+  },
+  result: {
+    type: String,
+    default: null
+  }
+}, { timestamps: true });
+
+export default mongoose.model("Bet", betSchema);
