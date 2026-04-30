cf08c5fcd5fc7a30e9b5b8f9ca3a886e9d95d4c7
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 22:17:44 +0600

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const transactionSchema = new mongoose.Schema({
+  userId: String,
+  type: String, // deposit, withdraw, win, lose
+  amount: Number,
+  status: { type: String, default: "success" },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Transaction", transactionSchema);
