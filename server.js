4a05aae809060d2211bf3dc7c37877fec8eefcdd
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 22:14:57 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
+const app = express();
+
+// DB connect
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/wallet", require("./routes/walletRoutes"));
+app.use("/api/game", require("./routes/gameRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("MEGARBAJEE API RUNNING 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on", PORT));
