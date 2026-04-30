9ed1dc03aaa324331f550c23e970f1d31183b252
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 23:06:02 +0600

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,14 @@
+export const successResponse = (res, data, message = "Success") => {
+  return res.json({
+    success: true,
+    message,
+    data
+  });
+};
 
+export const errorResponse = (res, message = "Error", code = 500) => {
+  return res.status(code).json({
+    success: false,
+    message
+  });
+};
