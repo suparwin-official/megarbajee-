1db721b6411a5e0ede4734b9f476682915ebeda1
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 22:15:16 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB Connected 🚀");
+  } catch (err) {
+    console.log("DB Error:", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
