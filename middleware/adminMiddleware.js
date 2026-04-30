6b980c930c0a91e83ada9cb3a1a690fa524d70aa
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 23:12:13 +0600

    Update adminMiddleware.js

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,1 +1,16 @@
+export const adminMiddleware = (req, res, next) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
+    if (req.user.role !== "admin") {
+      return res.status(403).json({ message: "Admin only access" });
+    }
+
+    next();
+
+  } catch (err) {
+    return res.status(500).json({ message: "Server error" });
+  }
+};
