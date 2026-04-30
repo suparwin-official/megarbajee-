d5dd7f2db1523032cc7faf1fe32aac216ae41c5d
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 22:15:36 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  username: { type: String, unique: true },
+  password: String,
+  role: { type: String, default: "user" }
+});
+
+module.exports = mongoose.model("User", userSchema);
