0174b09c7bf06501085226d0b048124b0cdd2f8e
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 23:12:29 +0600

    Add error handling middleware

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,1 +1,8 @@
+export const errorMiddleware = (err, req, res, next) => {
+  console.error(err.stack);
 
+  res.status(500).json({
+    success: false,
+    message: err.message || "Server Error"
+  });
+};
