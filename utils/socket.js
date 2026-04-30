458ca7c98a799ce1ac82c12679ce2a24075ac35e
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-30 23:05:47 +0600

    Update socket.js

--- utils/socket.js
+++ utils/socket.js
@@ -1,1 +1,28 @@
 
+import { Server } from "socket.io";
+
+export const initSocket = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*"
+    }
+  });
+
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    // place bet
+    socket.on("placeBet", (data) => {
+      io.emit("newBet", data);
+    });
+
+    // game result
+    socket.on("gameResult", (data) => {
+      io.emit("result", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+  });
+};
